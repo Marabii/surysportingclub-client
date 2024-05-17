490982bb27683719db626598379b1c5295cb3f92
Marabii <minehamza97@gmail.com>
2024-05-17 20:37:09 +0200

    Your commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+if (import.meta.env.VITE_REACT_APP_NODE_ENV !== "production")
+  disableReactDevTools();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
